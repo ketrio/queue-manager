fed7b6d4429b45944846d244dc7cc7375af95aab
Vlad <vladgutkovskii@gmail.com>
2018-02-13 01:07:45 +0300

    Add shiftDay function, minor getSubjects fix

--- src/API.js
+++ src/API.js
@@ -1,31 +1,8 @@
-function getSubjects(subgroup) {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status == 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        const weekNumber = response["currentWeekNumber"];
-        const schedules = response["schedules"].map(schedule =>
-          schedule.schedule.filter(e => [0, subgroup].includes(e.numSubgroup))
-        );
-        resolve({ weekNumber, schedules });
-      }
-    };
-    request.open(
-      "GET",
-      "https://students.bsuir.by/api/v1/studentGroup/schedule?studentGroup=653505",
-      true
-    );
-    request.setRequestHeader("Accept", "application/json");
-    request.send();
-  });
-}
-
-const filterWeeknum = (schedules, weekNumber) =>
-  schedules.map(schedule =>
-    schedule.filter(subject => subject["weekNumber"].includes(weekNumber))
-  );
+const shiftDay = shift => {
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + shift);
+  return currentDate;
+};
 
 const clear = schedules =>
   schedules.map(schedule =>
@@ -36,3 +13,41 @@ const clear = schedules =>
       };
     })
   );
+
+const filterWeeknum = (schedules, weekNumber) =>
+  schedules.map(schedule =>
+    schedule.filter(subject => subject['weekNumber'].includes(weekNumber))
+  );
+
+const getSubjects = subgroup => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status == 200) {
+        const response = JSON.parse(this.responseText);
+        const weekNumber = response['currentWeekNumber'];
+        const schedules = response['schedules']
+          .slice(new Date().getDay() - 1 || 1)
+          .map(schedule =>
+            schedule.schedule.filter(
+              e =>
+                [0, subgroup].includes(e.numSubgroup) && e.lessonType === 'ЛР'
+            )
+          )
+          .map((schedule, i) => {
+            return { date: shiftDay(i), schedule };
+          });
+        resolve({ weekNumber, schedules });
+      }
+    };
+    request.open(
+      'GET',
+      'https://students.bsuir.by/api/v1/studentGroup/schedule?studentGroup=653505',
+      true
+    );
+    request.setRequestHeader('Accept', 'application/json');
+    request.send();
+  });
+};
+
+export { filterWeeknum, getSubjects };
---
+++ src/App.js
@@ -0,0 +1,3 @@
+import { filterWeeknum, getSubjects } from './API';
+
+getSubjects(1).then(console.log);
