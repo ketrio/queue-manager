4d404f9977438ddb036f3c4ddd3f145c5e08808c
Vlad <vladgutkovskii@gmail.com>
2018-02-13 18:24:56 +0300

    API done

--- src/API.js
+++ src/API.js
@@ -3,34 +3,28 @@ const filterWeeknum = (schedules, weekNumber) =>
     schedule.filter(subject => subject['weekNumber'].includes(weekNumber))
   );
 
-const getSubjects = subgroup => {
+const getSubjects = (subgroup, weekNumber) => {
   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
     request.onerror = reject;
     request.onreadystatechange = function() {
-      if (this.status >= 200 && this.status < 400) {
-        const response = JSON.parse(this.responseText);
+      if (this.readyState === 4 && this.status == 200) {
+        const response = JSON.parse(this.response);
         const weekNumber = response['currentWeekNumber'];
         const schedules = response['schedules']
           .slice(new Date().getDay() - 1 || 1)
-          .map(schedule =>
-            schedule.map(period => {
-              return {
-                subject: period.subject,
-                subgroup: period.numSubgroup,
-              };
-            })
-          )
           .map(schedule =>
             schedule.schedule.filter(
               e =>
                 [0, subgroup].includes(e.numSubgroup) && e.lessonType === 'ЛР'
             )
           )
-          .map((schedule, i) => {
+          .map(schedule => schedule.map(period => period.subject))
+          .filter(schedule => schedule.length > 0)
+          .map((subjects, i) => {
             const date = new Date();
             date.setDate(date.getDate() + i);
-            return { date, schedule };
+            return { date, subjects };
           });
         resolve({ weekNumber, schedules });
       }
--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
 import { filterWeeknum, getSubjects } from './API';
+import { shuffle, daterandom } from './random';
+import students from './students.json'
 
-getSubjects(1).then(console.log);
+getSubjects(1).then(res => {
+  const queue = res.schedules.map(schedule => {
+    const rnd = daterandom(schedule.date);
+    const order = schedule.subjects.map(subject => ({subject, line: shuffle(rnd, students)}));
+    return {order, date: schedule.date};
+  });
+  console.log(queue);
+});
